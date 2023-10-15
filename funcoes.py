648f757365f282e6d8666615f85dfa1fc34b7827
Artur Paiva <artur7paiva@hotmai.com>
2023-10-15 16:05:08 -0300

    Update de funcionalidades

--- funcoes.py
+++ funcoes.py
@@ -1,18 +1,66 @@
+from users import *
 def novo_cliente():
-    razao_social = input("Digite a sua razão social: ")
-    cnpj = input("Digite o seu CNPJ: ")
-    tipo_conta = input("Qual o seu tipo de conta? (comum/plus): ")
-    valor_inicial = float(input("Digite o seu valor inicial: "))
-    senha_usuario = input("Digite sua senha: ")
-    print("Razão social:", razao_social)
-    print("CNPJ:", cnpj)
-    print("Tipo da conta: ",tipo_conta)
-    print("Valor inicial:", valor_inicial)
-    print("Senha:", senha_usuario)
+    valores = [] #cria uma lista com os valores da chave do user
+    
+    razao_social = input("Informe sua razão social: ")
+    valores.append(razao_social) #adiciona a razao social à lista "valores"
+    
+    tipo_conta = input("Qual o seu tipo de conta? (comum/plus): ").lower()
+    while tipo_conta != "comum" and tipo_conta != "plus":
+        tipo_conta = input("Por favor insira uma opção correta do seu tipo de conta: ")
+    valores.append(tipo_conta)  #verifica o tipo da conta do usuario, e depois adiciona à lista "valores"
+    
+    while(True):
+        try:
+            valor_inicial = float(input("Informe o saldo inicial de sua conta: "))
+            break
+        except ValueError:
+            print("Valor inválido")
+    valores.append(valor_inicial) # pede ao usuario um input com o valor inicial de sua conta, caso o valor seja inválido, o except constata o erro e mostra ao user
+        
+    while(True):
+        try:
+            cnpj = int(input("Informe SOMENTE os números do seu cnpj: "))
+            break
+        except ValueError:
+            print("Seu cnpj é inválido, tente inserir novamente apenas com os números: ") # pede ao usuário o cnpj, caso o valor seja invalido, o except constata o erro e o mostra ao user
+    
+    senha = input("Crie uma senha com pelo menos 6 dígitos: ")
+    
+    while(True):
+        if len(senha) < 6:
+            senha = input("Crie uma senha com pelo menos 6 dígitos: ")
+        else:
+            break # pede a senha ao usuario com no minimo 6 digitos, caso seja menor, o programa faz ele responder de novo, ate informar uma senha válida
+
+    valores.append(senha)
+
+    if cnpj not in users:
+        valores = valores
+        users[cnpj] = valores
+        print("User cadastrado com sucesso, muito obrigado por fazer parte do nosso banco!")
+        return users
+    else:
+        print("Usuário ja cadastrado") # com o valor da tupla, o programa verifica se o usuario ainda nao existe no sistema, caso se confirme, um novo user é criado
 
 def apaga_cliente():
-    cnpj = input("Digite o seu CNPJ: ")
-    print(cnpj)
+    chaves = users.keys()
+    cnpj = int(input("Digite o seu CNPJ: ")) # O programa pergunta o cnpj para conferir se o cnpj é realmente uma chave valida em users
+    while(True):
+        if cnpj in chaves: # Caso o cnpj seja correto a próxima validação é a senha 
+            senha = input("Digite sua senha: ")
+            if senha in users[cnpj]: # Caso a senha seja correta o usuario é deletado com sucesso
+                print(users)                           
+                del users[cnpj]
+                print("Usuário deletado com sucesso!")
+                print(users)
+                break
+            else: # Caso a senha não seja correta o programa detecta e termina a operação avisando que o usuario ou senha são invalidos.
+                print("Usuario ou senha invalidos")
+                break
+        else:
+            print('Usuario não casdastrado') # Caso o usuario não esteja cadastrado, o programa identifica e retorna usuario não cadastrado, terminando a operação.
+            break
 
 def debito():
     cnpj = input("Digite o seu CNPJ: ")
