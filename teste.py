713e818d34155e1eefa3024dc0a5869e5ec66816
Giovanni Moreira <giovannimoreira131@gmail.com>
2023-10-24 21:17:56 -0300

    teste atualizacao

--- teste.py
+++ teste.py
@@ -1,9 +1,11 @@
 dados = {
     'pessoa1': {
+        'cnpj': '123',
         'nome': 'Alice',
         'idade': 30
     },
     'pessoa2': {
+        'cnpj': '321',
         'nome': 'Bob',
         'idade': 25
     },
@@ -16,20 +18,26 @@ dados = {
 with open('dados.txt', 'w') as arquivo:
     # Percorra as chaves do dicionário principal (neste caso, as pessoas)
     for pessoa, info in dados.items():
-        arquivo.write(f'Pessoa: {pessoa}\n')
+        arquivo.write(f'pessoa: {pessoa}\n')
         # Percorra as chaves e valores dos dicionários internos (nome e idade)
         for chave, valor in info.items():
             arquivo.write(f'{chave}: {valor}\n')
         arquivo.write('\n')  # Adicione uma linha em branco entre cada pessoa
-        
+
+
+cnpj = input("Digite seu cnpj: ")     
 with open('dados.txt', 'r') as arquivo:
     # Leia o arquivo linha por linha
     for linha in arquivo:
-        # Divida a linha em palavras separadas por dois pontos (':')
         partes = linha.strip().split(': ')
-        # Verifique se a primeira parte da linha é "nome" e a segunda parte é "Bob"
-        if len(partes) == 2 and partes[0] == 'nome' and partes[1] == 'Bob':
-            # Quando encontrar o nome "Bob", leia a próxima linha (idade)
-            idade = next(arquivo).strip()
-            print(f'A idade de Bob é: {idade}')
-            break  # Pare a busca após encontrar a informação desejada
+    
+        if len(partes) == 2 and partes[0] == 'cnpj' and partes[1] == cnpj:
+            if len(partes) == 2 and partes[0] == 'cnpj':
+                print(partes[1])
+            elif len(partes) == 2 and partes[0] == 'nome':
+                print(partes[1])
+            elif len(partes) == 2 and partes[0] == 'idade':
+                print(partes[1])
+        
+            
+        
