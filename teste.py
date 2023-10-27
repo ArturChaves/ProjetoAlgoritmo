64aed1aa71071970fec40dfdad767860e020d006
GiovanniMoreira1 <giovannimoreira131@gmail.com>
2023-10-27 16:52:14 -0300

    função adicionar cliente e dados funcionando

--- teste.py
+++ teste.py
@@ -1,24 +1,24 @@
 import json
 
-with open("dados.json", "a") as arquivo_json:
-    if arquivo_json.tell() > 0:
-        arquivo_json.write("\n")
 
-    cnpj = input("CNPJ: ")
-    razao_social = input("Razão social: ")
-    saldo = float(input("saldo inicial:"))
-    tipo_conta = input("tipo da conta(comum/plus): ").lower()
-    senha = input("senha: ")
+with open("dados.json", "r") as arquivo_json:
+    dados = json.load(arquivo_json)
 
-    dados = {
-        cnpj: {
+cnpj = input("CNPJ: ")
+razao_social = input("Razão social: ")
+saldo = float(input("saldo inicial:"))
+tipo_conta = input("tipo da conta(comum/plus): ").lower()
+senha = input("senha: ")
+
+dados[cnpj] = {
         "razao_social": razao_social,
         "saldo" : saldo,
         "tipo_conta" : tipo_conta,
         "senha" : senha
     }
-    }
 
+    
+with open("dados.json", "w") as arquivo_json:
     json.dump(dados, arquivo_json, indent=4)
 
 
