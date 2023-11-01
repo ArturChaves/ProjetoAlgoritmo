30ff8edd4cd0ee725226b5b9193cf1a80864ce34
GiovanniMoreira1 <giovannimoreira131@gmail.com>
2023-11-01 19:06:42 -0300

    comentarios

--- funcoes.py
+++ funcoes.py
@@ -12,7 +12,7 @@ def validar(cnpj):
             if senha == dados[str(cnpj)]["senha"]:  # Verifica se a senha corresponde ao CNPJ
                 return True
             tentativa += 1
-            if tentativa == 3:
+            if tentativa == 3:  # caso o user erre a senha 3 vezes, o programa precisará reiniciar
                 print("Reinicie o programa e tente novamente.")
                 break
         else:
@@ -33,7 +33,7 @@ def login():
     
         if login == '1':
             cnpj = int(input("Digite o seu CNPJ: "))
-            if validar(cnpj) == True:
+            if validar(cnpj) == True:   # faz a validação por meio da função e permite o login do user
                 break
             else:
                 print("Usuário não cadastrado, tente novamente.")
@@ -53,7 +53,7 @@ def novo_cliente():
         cnpj = int(input("CNPJ: "))
         cnpj = str(cnpj)
         validacao_cnpj = True
-        if len(cnpj) == 3:
+        if len(cnpj) == 14:      # aqui faz a comparação para saber o tamanho do cnpj, caso seja 14, está liberado para continuar as proximas etapas
             for contas in dados.keys():
                 if cnpj in contas:
                     validacao_cnpj = False
@@ -95,24 +95,24 @@ def novo_cliente():
             "tipo_conta" : tipo_conta,
             "senha" : senha,
             "transacoes": []
-        }
+        }       # esse é o modelo do dicionario que consta no arquivo .json
         
     with open("dados.json", "w") as arquivo_json:
-        json.dump(dados, arquivo_json, indent=4)
+        json.dump(dados, arquivo_json, indent=4) # com esse comando, esse dicionario é escrito no json dessa maneira
 
 def apaga_cliente():
 
     cnpj = int(input("Digite o seu CNPJ: "))
 
     with open("dados.json", "r") as arquivo_json:
-        dados = json.load(arquivo_json)
+        dados = json.load(arquivo_json)     # o with open("dados","r") serve para fazer a leitura do dicionario, o load puxa esses dados e transforma em um dicionario que pode ser manipulado no python
 
     if str(cnpj) in dados:
         del dados[str(cnpj)]
 
         with open("dados.json", "w") as arquivo_json:
             json.dump(dados, arquivo_json, indent=4)
-        print("Cliente removido com sucesso!")
+        print("Cliente removido com sucesso!")  # o arquivo muda os valores que se pede e reescreve o dicionario e passa ao arquivo json
     else:
         print("CNPJ não encontrado, tente novamente com um valor válido!")
 
@@ -141,11 +141,11 @@ def debito():
     if cnpj in dados:            
         try:
             valor = float(input("Digite o valor que será debitado: "))  # Pede ao usuário o valor que será debitado
-            if valor <= dados[cnpj]["saldo"]:
+            if valor <= dados[cnpj]["saldo"]: # caso o valor que o user deseje ser debitado seja mais alto que o seu saldo atual, o cliente nao consegue continuar a operação
                 dados[cnpj]["saldo"] -= valor
                 historico = data_atual()
                 lista = ["Debito", valor, dados[cnpj]["saldo"],historico]
-                dados[cnpj]["transacoes"].append(lista)
+                dados[cnpj]["transacoes"].append(lista) # esse .append(lista) serve para adicionar as transações na conta do user, e será usado para fazer o extrato
                 with open("dados.json", "w") as arquivo_json:
                     json.dump(dados, arquivo_json, indent=4)
                 print("Valor debitado com sucesso, o seu saldo atual é de:", dados[cnpj]["saldo"])
@@ -160,7 +160,7 @@ def debito():
 
 def deposito():
     with open("dados.json", "r") as arquivo_json:
-        dados = json.load(arquivo_json)
+        dados = json.load(arquivo_json) # este comando segue a mesma logica da função débito, o programa lê o dicionario, passa para o python que faz as mudanças necessárias e logo após disso, o reescreve novamente em formato .json
 
     cnpj = input("Digite o seu CNPJ: ")
     
@@ -189,7 +189,7 @@ def transferencia_entre_contas():
         destino = input("Digite o cnpj do destinatário: ")
         valor = float(input("Digite o valor da transfêrencia: "))
         if valor < dados[cnpj]["saldo"]:
-            dados[cnpj]["saldo"] -= valor     # O valor é debitado da conta do remetente e adicionado à conta do destinatário
+            dados[cnpj]["saldo"] -= valor     # O valor é debitado da conta do remetente e adicionado à conta do destinatário, e a transação fica salva no extrato dos dois clientes envolvidos na transferência
             historico = data_atual()
             lista = ["Transferencia", valor, dados[cnpj]["saldo"], historico]
             lista_destino = ["Transferencia", -valor, dados[destino]["saldo"], historico]
